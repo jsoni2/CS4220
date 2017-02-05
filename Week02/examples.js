02a5ef5f2323fdcd063f10caf57dd07457443bc1
Auman <cydney.m.auman@jpl.nasa.gov>
2017-02-05 11:37:03 -0800

    week02 dir.

---
+++ Week02/examples.js
@@ -0,0 +1,57 @@
+// NUMBERS
+console.log(parseInt('5'))     // 5
+console.log(parseInt('5.5'))   // 5
+console.log(parseInt('hello')) // NaN
+
+console.log(parseFloat('5'))     // 5
+console.log(parseFloat('5.5'))   // 5.5
+console.log(parseFloat('hello')) // NaN
+
+// STRINGS
+const str = 'HellO'
+
+const spaced = '    hello world  '
+
+const greeting = 'hello world'
+
+
+// OBJECTS
+const pet = {
+    type: 'dog',
+    breed: 'border collie',
+    colors: ['black', 'white']
+}
+
+
+// ARRAYS
+const alpha = ['a', 'b', 'c']
+
+const list = [ 'red', 'blue', 'green', 'yellow']
+
+
+// DESTRUCTURING
+const arr = [ 1, 2, 3, 4 ]
+
+const fido = {
+    type: 'dog',
+    breed: 'border collie',
+    colors: ['black', 'white']
+}
+
+
+// TEMPLATE LITERALS
+const foods = [ 'pizza', 'pasta', 'sushi' ]
+
+
+// CLASSES
+class Polygon {
+    constructor(height, width) {
+        this.height = height
+        this.width = width
+    }
+}
+
+
+
+
+
