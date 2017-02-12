cf4c9b961525a7a5091d7c92de7d636c6358deff
Auman <cydney.m.auman@jpl.nasa.gov>
2017-02-12 15:56:13 -0800

    week03

---
+++ Week03/examples.js
@@ -0,0 +1,48 @@
+// // TIMERS
+console.log('Before')
+console.log('After')
+
+console.log('Before')
+let count = 0
+console.log('After')
+
+
+// // CALLBACKS
+const foods = [
+        { food: 'pizza', time: 3000 },
+        { food: 'spaghetti', time: 2000 },
+        { food: 'sandwich', time: 1000 }
+    ]
+
+const cook = ({ food, time }, callback) => {
+
+}
+
+
+// // CALLBACKS WITH ERRORS
+const oddEven = (n, callback) => {
+}
+
+
+
+// // PROMISES
+const cookAgain = ({ food, time }) => {
+}
+
+
+// // LOOP PROMISES
+const makeFoodAgain = (foods) => {
+}
+
+
+// // PROMISE ALL
+const makeAllFoods = (foods) => {
+}
+
+// // PROMISES WITH REJECTIONS
+const numbers = [2, 4, 5, 8, 10]
+}
+
+
+
+
