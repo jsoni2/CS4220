947bc6846657fc6c4eaca7c7440751b867f4a010
Auman <cydney.m.auman@jpl.nasa.gov>
2017-02-23 11:34:32 -0800

    add help option to cli.js

--- Week04/calculator-cli/cli.js
+++ Week04/calculator-cli/cli.js
@@ -3,11 +3,19 @@ const
     quiz = require('./app')
 
 const flags = yargs.usage('$0: usage --run')
+    .options('h', {
+        alias: 'help',
+        describe: 'displays help'
+    })
     .options('r', {
         alias: 'run',
         describe: 'run the quiz'
     })
     .argv
+
+
+if (flags.help)
+    yargs.showHelp()
 
 if (flags.run)
     quiz.run()
