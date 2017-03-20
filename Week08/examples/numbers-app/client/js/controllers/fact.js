6d4c2e807c26a37230c5bfb56483a3b64558b304
Janak Soni <jakzi7005@gmail.com>
2017-03-20 14:14:15 -0700

    Added trivia, date and random fact primarily

--- Week08/examples/numbers-app/client/js/controllers/fact.js
+++ Week08/examples/numbers-app/client/js/controllers/fact.js
@@ -10,4 +10,28 @@ angular
                 $scope.fact = response
             })
         }
+        $scope.getTriviaFact = () => {
+            FactService.get({
+                type: 'trivia',
+                number: $scope.number,
+            }, (response) => {
+                $scope.fact = response
+            })
+        }
+        $scope.getDateFact = () => {
+            FactService.get({
+                type: 'date',
+                number: $scope.number,
+            }, (response) => {
+                $scope.fact = response
+            })
+        }
+        $scope.getRandomFact = () => {
+            FactService.get({
+                type: 'math',
+                number: $scope.number,
+            }, (response) => {
+                $scope.fact = response
+            })
+        }
     })
