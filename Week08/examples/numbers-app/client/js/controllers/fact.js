4f4ff018085a0d49f265758710307b2c44ae9223
Janak Soni <jakzi7005@gmail.com>
2017-03-21 09:29:50 -0700

    lab2

--- Week08/examples/numbers-app/client/js/controllers/fact.js
+++ Week08/examples/numbers-app/client/js/controllers/fact.js
@@ -1,37 +1,88 @@
 angular
-    .module('FactCtrl', [])
-    .controller('FactController', function($scope, FactService) {
+.module('FactCtrl', [])
+.controller('FactController', function($scope, FactService) {
 
-        $scope.getMathFact = () => {
-            FactService.get({
-                type: 'math',
-                number: $scope.number,
-            }, (response) => {
-                $scope.fact = response
-            })
-        }
-        $scope.getTriviaFact = () => {
-            FactService.get({
-                type: 'trivia',
-                number: $scope.number,
-            }, (response) => {
-                $scope.fact = response
-            })
-        }
-        $scope.getDateFact = () => {
-            FactService.get({
-                type: 'date',
-                number: $scope.number,
-            }, (response) => {
-                $scope.fact = response
-            })
-        }
-        $scope.getRandomFact = () => {
-            FactService.get({
-                type: 'math',
-                number: $scope.number,
-            }, (response) => {
-                $scope.fact = response
-            })
-        }
+  $scope.getMathFact = () => {
+    FactService.get({
+      type: 'math',
+      number: $scope.number,
+    }, (response) => {
+      if (response.found) {
+        $scope.fact = response
+      }
+      else {
+        $scope.fact = ''
+      }
     })
+  }
+  $scope.getTriviaFact = () => {
+    FactService.get({
+      type: 'trivia',
+      number: $scope.number,
+    }, (response) => {
+      if (response.found) {
+        $scope.fact = response
+      }
+      else {
+        $scope.fact = ''
+      }
+    })
+  }
+  $scope.getDateFact = () => {
+    let fullDate=$scope.number.split('/')
+    if (fullDate[2]) {
+      FactService.get({
+        type: 'year',
+        number: fullDate[2],
+      }, (response) => {
+        if (response.found) {
+          $scope.fact = response
+          $scope.fact.number=$scope.number
+        }
+        else {
+          $scope.fact = ''
+        }
+      })
+    }
+    else {
+      FactService.get({
+        type: 'date',
+        number: $scope.number,
+      }, (response) => {
+        if (response.found) {
+          $scope.fact = response
+          $scope.fact.number=$scope.number
+        }
+        else {
+          $scope.fact = ''
+        }
+      })
+    }
+  }
+  $scope.getRandomFact = () => {
+    const randomTypes=['math','trivia','date']
+    let randomType=randomTypes[Math.floor(Math.random()*randomTypes.length)]
+    if (randomType!=='date') {
+      let randomNumber=Math.floor(Math.random() * 100) + 1
+      FactService.get({
+        type: randomType,
+        number: randomNumber,
+      }, (response) => {
+        $scope.fact = response
+      })
+    }
+    else {
+      let randomMonth=Math.floor(Math.random() * 12) + 1
+      let randomDay=Math.floor(Math.random() * 31) + 1
+      let randomDate=randomMonth+'/'+randomDay
+      FactService.get({
+        type: 'date',
+        number: randomDate,
+        //random: true
+      }, (response) => {
+        $scope.fact = response
+        $scope.fact.number=randomDate
+      })
+    }
+  }
+})
